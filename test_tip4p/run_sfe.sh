350b72157c208a5943059f1374872599a88b367e
Barbara Morales <bamo6610@colorado.edu>
2024-09-10 10:47:25 -0600

    SFE calculation results for tip4p water model

--- test_tip4p/run_sfe.sh
+++ test_tip4p/run_sfe.sh
@@ -13,11 +13,11 @@
 module purge
 module avail
 ml anaconda
-conda activate old-evaluator-test7
+conda activate old-evaluator-vsites
 
 function convert_ff_to_v3 () {
-    FF="/projects/bamo6610/software/anaconda/env/old-evaluator-test7/lib/python3.9/site-packages/openforcefields/offxml/$1"
-    cp /projects/bamo6610/software/anaconda/env/old-evaluator-test7/lib/python3.9/site-packages/openforcefields/offxml/$1 $FF
+    FF="/projects/bamo6610/software/anaconda/env/old-evaluator-vsites/lib/python3.9/site-packages/openforcefields/offxml/$1"
+    cp /projects/bamo6610/software/anaconda/env/old-evaluator-vsites/lib/python3.9/site-packages/openforcefields/offxml/$1 $FF
     sed -i 's/    <vdW version="0.4" potential="Lennard-Jones-12-6" combining_rules="Lorentz-Berthelot" scale12="0.0" scale13="0.0" scale14="0.5" scale15="1.0" cutoff="9.0 \* angstrom \*\* 1" switch_width="1.0 \* angstrom \*\* 1" periodic_method="cutoff" nonperiodic_method="no-cutoff">/    <vdW version="0.3" potential="Lennard-Jones-12-6" combining_rules="Lorentz-Berthelot" scale12="0.0" scale13="0.0" scale14="0.5" scale15="1.0" cutoff="9.0 \* angstrom" switch_width="1.0 \* angstrom" method="cutoff">/g' $FF
     sed -i 's/    <Electrostatics version="0.4" scale12="0.0" scale13="0.0" scale14="0.8333333333" scale15="1.0" cutoff="9.0 \* angstrom \*\* 1" switch_width="0.0 \* angstrom \*\* 1" periodic_potential="Ewald3D-ConductingBoundary" nonperiodic_potential="Coulomb" exception_potential="Coulomb">/    <Electrostatics version="0.3" scale12="0.0" scale13="0.0" scale14="0.8333333333" scale15="1.0" cutoff="9.0 \* angstrom" switch_width="0.0 \* angstrom" method="PME">/g' $FF
     echo "converted $FF"
